73046efff80001cc495bcb036ef0ce0b05e6ecb8
Mads Henrichsen <mgh@nine.dk>
2018-03-09 16:06:19 +0100

    Tensorflow ML, og hjemmeside tilføjet

---
+++ rewrite for html.py
@@ -0,0 +1,5 @@
+import os
+os.listdir("/Users/madsgadehenrichsen/WebstormProjects/Melanoma/Melanoma")
+
+for  billed in os.listdir("/Users/madsgadehenrichsen/WebstormProjects/Melanoma/Modermaerker"):
+    print "'../Modermaerker/"+billed+"',"
